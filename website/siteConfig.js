c4c627c817bfe9063319306c3964ed99740807b8
Milan Kolar <milan@pype.club>
2019-12-07 13:41:28 +0100

    fix circleci

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -94,7 +94,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: '',
+  projectName: 'pypeclub.github.io',
   organizationName: 'pypeclub',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
