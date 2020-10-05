170b6a3d1c9ce48d086cd2607b143a1cfaf7c9e4
Milan Kolar <milan@pype.club>
2020-10-06 00:31:58 +0200

    get features on par with v1

--- website_v2/src/pages/features.js
+++ website_v2/src/pages/features.js
@@ -82,28 +82,177 @@ const ftrack = [
   }
 ];
 
-function Feature({link, label, description}) {
-  return (
-    <div className={styles.card}>
-      <div className="card">
-        <div className="card__header">
-          <h4>{label}</h4>
-        </div>
-        <div className="card__body">
+const ftrackActions = [
+  {label: "Launch Application"},
+  {link: "", label: "Review in RV Player"},
+  {link: "", label: "Review in DJV view"},
+  {link: "", label: "Propagate Thumbnails"},
+  {link: "", label: "Prepare New Project"},
+  {link: "", label: "Delete Old Subsets"},
+  {link: "", label: "Create Folders"},
+  {link: "", label: "Open Location"},
+  {link: "", label: "Kill old ftrack jobs"},
+  {link: "", label: "Sort Client Review"},
+  {link: "", label: "Start Next Task"},
+  {link: "", label: "Version to Task Status"},
+  {link: "", label: "Autosync to Pype DB"}
+];
+
+
+const maya_features = [
+
+  {
+    label: "Look Management",
+    link: "docs/artist_hosts_maya#look-development",
+    description:"Publish maya shading networks with textures and assign them to all assets in the scene at once"
+  }, {
+    label: "Project Shelves",
+    description:"Add any custom projects scripts to dynamically generated maya shelves"
+  },
+  {
+    label: "Playblasts",
+    link: "docs/artist_hosts_maya#reviews",
+    description:"Makes sure all your playblasts are consistent, with burnins and and correct viewport settings"
+  },
+  {
+    label: "Model > Render",
+    description:"We cover full project data flow from model throght animation, till final render."
+  },
+  {
+    label: "Plugins Support",
+    description:"Full production workflows support for Arnold, Vray, Redshift and Yeti. With more added upon client requests."
+  }
+]
+
+const maya_families = [
+    {label:"Model"},
+    {label:"Look"},
+    {label:"Rig"},
+    {label:"Setdress"},
+    {label:"Animation"},
+    {label:"Point Cache"},
+    {label:"VDB Cache"},
+    {label:"Assembly"},
+    {label:"Camera"},
+    {label:"RenderSetup"},
+    {label:"Render"},
+    {label:"Plate"},
+    {label:"Review"},
+    {label:"Workfile"},
+    {label:".ASS StandIn"},
+    {label:"Yeti Cache"},
+    {label:"Yeti Rig"},
+    {label:"Vray Scene"},
+    {label:"Vray Proxy"}
+]
+
+const nuke_features = [
+
+  {
+    label: "Color Managed",
+    description:"Fully colour managed outputs for work and review."
+  }, {
+    label: "Script Building",
+    description:"Automatically build first workfiles from published plates or renders."
+  },
+  {
+    label: "Node Presets",
+    description:"Template system for centrally controlled node parameters."
+  },
+  {
+    label: "Model > Render",
+    description:"We cover full project data flow from model throght animation, till final render."
+  },
+  {
+    label: "Data ingest",
+    description:"Create shots and sequences from nuke studio timeline, inlcuding all metadata."
+  }
+]
+
+const nuke_families = [
+    {label: "Model"},
+    {label: "Camera"},
+    {label: "Render"},
+    {label: "Plate"},
+    {label: "Review"},
+    {label: "Group"},
+    {label: "Workfile"},
+    {label: "LUT"},
+    {label: "Cache"}
+]
+
+
+
+class FeatureKey extends React.Component {
+  render() {
+    const label = this.props.label
+    const link = (this.props.link || "")
+    const description = this.props.description
+    return (
+        <div className="card">
+          <div className="card__header">
+            <h4>{label}</h4>
+          </div>
+          <div className="card__body">
           <p>
             {description}
           </p>
+          </div>
+          <div className={classnames(styles.more_item,
+                                     "card__footer")}>
+            {link != "" &&
+              <a href={link}>MORE <i className="fas fa-chevron-right"></i></a>
+            }
+          </div>
         </div>
-        <div className={classnames(styles.more_item,
-                                   "card__footer")}>
-          {link != "" &&
-            <a href={link}>MORE <i className="fas fa-chevron-right"></i></a>
-          }
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
+class FeatureMedium extends React.Component {
+  render() {
+    const label = this.props.label
+    const link = (this.props.link || "")
+    const description = this.props.description
+    return (
+        <div className="card card_medium">
+          <div className="card__header">
+            <h4>{label}</h4>
+          </div>
+          <div className="card__body">
+          <p>
+            {description}
+          </p>
+          </div>
+          <div className={classnames(styles.more_item,
+                                     "card__footer")}>
+            {link != "" &&
+              <a href={link}>MORE <i className="fas fa-chevron-right"></i></a>
+            }
+          </div>
+        </div>
+    );
+  }
+}
+
+class FeatureSmall extends React.Component {
+  render() {
+    const label = this.props.label
+    const link = (this.props.link || "")
+    const description = this.props.description
+
+    return (
+      <div className="card card_small">
+
+        <div className="card__body">
+        <h5>{label}</h5>
+        </div>
+
+      </div>
+    );
+  }
+}
+
 
 
 function Home() {
@@ -114,31 +263,99 @@ function Home() {
       title={`${siteConfig.title}- Features`}
       description="Pype Feature list">
 
-      {key_features && key_features.length && (
-        <section className={classnames("section")}>
-          <div className={classnames(styles.card_container, "container")}>
-            <h2>Key Features</h2>
+      <section className={classnames("section lightBackground")}>
+        <div className={classnames(styles.card_container, "container")}>
+          <h2>Key Features</h2>
+
+          {key_features && key_features.length && (
             <div className={styles.card_box}>
               {key_features.map((props, idx) => (
-                <Feature key={idx} {...props} />
+                <FeatureKey key={idx} {...props} />
               ))}
             </div>
-          </div>
-        </section>
-      )}
+          )}
+        </div>
+      </section>
 
-      {ftrack && ftrack.length && (
-        <section className={classnames("section darkBackground")}>
-          <div className={classnames(styles.card_container, "container")}>
-            <h2>Ftrack</h2>
+
+
+      <section className={classnames("section darkBackground")}>
+        <div className={classnames(styles.card_container, "container")}>
+          <h2>Ftrack</h2>
+
+          {ftrack && ftrack.length && (
             <div className={styles.card_box}>
               {ftrack.map((props, idx) => (
-                <Feature key={idx} {...props} />
+                <FeatureMedium key={idx} {...props} />
               ))}
             </div>
-          </div>
-        </section>
-      )}
+          )}
+
+          <h3> Actions and Events</h3>
+
+          {ftrack && ftrack.length && (
+            <div className={styles.card_box}>
+              {ftrackActions.map((props, idx) => (
+                <FeatureSmall key={idx} {...props} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+
+      <section className={classnames("section lightBackground")}>
+        <div className={classnames(styles.card_container, "container")}>
+
+          <h2>Autodesk Maya</h2>
+          <p className="sectionDescription">versions 2017 and higher</p>
+
+          {maya_features && maya_features.length && (
+            <div className={styles.card_box}>
+              {maya_features.map((props, idx) => (
+                <FeatureMedium key={idx} {...props} />
+              ))}
+            </div>
+          )}
+
+          <h3 className=""> Supported Families </h3>
+
+          {maya_families && maya_families.length && (
+            <div className={styles.card_box}>
+              {maya_families.map((props, idx) => (
+                <FeatureSmall key={idx} {...props} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+
+      <section className={classnames("section darkBackground")}>
+        <div className={classnames(styles.card_container, "container")}>
+          <h2>Foundry Nuke | Nuke Studio | Hiero</h2>
+
+          <p className="sectionDescription">versions 11.0 and higher</p>
+
+          {nuke_features && nuke_features.length && (
+            <div className={styles.card_box}>
+              {nuke_features.map((props, idx) => (
+                <FeatureMedium key={idx} {...props} />
+              ))}
+            </div>
+          )}
+
+          <h3 className=""> Supported Families </h3>
+
+          {nuke_families && nuke_families.length && (
+            <div className={styles.card_box}>
+              {nuke_families.map((props, idx) => (
+                <FeatureSmall key={idx} {...props} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+
+
 
     </Layout>
     );
